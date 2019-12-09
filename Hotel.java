f4a77d6f265ba6bd53f19ea87908618ee2a7f652
trsjulie <37087267+trsjulie@users.noreply.github.com>
2019-12-09 11:55:07 +0100

    Add files via upload

---
+++ Hotel.java
@@ -0,0 +1,13 @@
+public class Hotel{
+    private String name;
+    private Simple[] etage_simple;
+    private CDouble[] etage_double;
+    private Deluxe[] etage_deluxe;
+    private Triple[] etage_triple;
+    private ArrayList<Client> liste_clients_simple;
+    private ArrayList<Client> liste_clients_double;
+    private ArrayList<Client> liste_clients_triple;
+    private ArrayList<Client> liste_clients_deluxe;
+
+
+}
